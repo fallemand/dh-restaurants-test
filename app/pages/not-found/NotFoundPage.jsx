14558a4aaf900c1e6a9b6cfda56e67882f5b6a90
Facundo Allemand <facundo.allemand@mercadolibre.com>
2018-10-02 14:51:16 -0300

    simplify not_found page using error page.

--- app/pages/not-found/NotFoundPage.jsx
+++ app/pages/not-found/NotFoundPage.jsx
@@ -1,20 +1,15 @@
 import React from 'react';
-import './_not-found.scss';
-import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import ErrorPage from '../error/ErrorPage';
 
 const NotFoundPage = ({ history }) => (
-  <div className="not-found">
-    <h1 className="not-found__title">404</h1>
-    <h2 className="not-found__subtitle">We couldn&#x27;t find the page you&#x27;re looking for :(</h2>
-    <button
-      className="not-found__button"
-      type="button"
-      onClick={() => history.push('/restaurants')}
-    >
-      Go Home!
-    </button>
-  </div>
+  <ErrorPage
+    title="404"
+    message="We couldn't find the page you're looking for :("
+    buttonLabel="Go Home!"
+    onClick={() => history.push('/restaurants')}
+  />
 );
 
 NotFoundPage.propTypes = {
